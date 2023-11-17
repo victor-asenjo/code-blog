5590f3091505f5d582eb34a0214b4118b97b70f8
Víctor Asenjo <victorasenj@gmail.com>
2023-11-17 19:42:24 +0100

    wip routes

---
+++ src/constantes/routes.js
@@ -0,0 +1,10 @@
+import Notes from '../pages/Notes';
+import Create from '../pages/Create';
+
+const routes = [
+  { path: '/', element: <Notes /> },
+  { path: '/create', element: <Create /> },
+  // Add more routes as needed
+];
+
+export default routes;
