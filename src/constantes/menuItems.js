759cc5f5036d84744b8edfff1e777574463d1182
Víctor Asenjo <victorasenj@gmail.com>
2023-11-17 22:39:13 +0100

    Update menuItems.js

--- src/constantes/menuItems.js
+++ src/constantes/menuItems.js
@@ -1,37 +1,44 @@
 import React from 'react';
-import { SubjectOutlined, AddCircleOutline, Code, QuestionAnswer } from '@mui/icons-material';
+import { SubjectOutlined, QuestionAnswer } from '@mui/icons-material';
+// React Icons
+import { DiPython, DiReact, DiJava, DiCode } from "react-icons/di";
+import { AiFillRobot } from "react-icons/ai";
+import { GiKeyboard } from "react-icons/gi";
+import { HiAcademicCap } from "react-icons/hi";
+
+const iconSize = 24; // Set the desired size for your icons
 
 const menuItem = [
   {
-    text: 'Languages CheatSheets',
-    icon: <SubjectOutlined />,
+    text: 'CheatSheets',
+    icon: <SubjectOutlined style={{ fontSize: iconSize }} />,
     path: '/',
     children: [
-      { text: 'Python', path: '/python', icon: <Code /> },
-      { text: 'Java', path: '/java', icon: <Code /> },
-      { text: 'React', path: '/react', icon: <Code /> },
+      { text: 'Python', path: '/python', icon: <DiPython size={iconSize} /> },
+      { text: 'Java', path: '/java', icon: <DiJava size={iconSize} /> },
+      { text: 'React', path: '/react', icon: <DiReact size={iconSize} /> },
     ],
   },
   {
     text: 'IAs',
-    icon: <QuestionAnswer />,
+    icon: <AiFillRobot style={{ fontSize: iconSize }} />,
     path: '/ias',
     children: [
-      { text: 'Text', path: '/ias/text', icon: <SubjectOutlined /> },
+      { text: 'Text', path: '/ias/text', icon: <SubjectOutlined style={{ fontSize: iconSize }} /> },
     ],
   },
   {
     text: 'Practice',
-    icon: <Code />,
+    icon: <HiAcademicCap style={{ fontSize: iconSize }} />,
     path: '/practice',
     children: [
-      { text: 'Code', path: '/practice/code', icon: <Code /> },
-      { text: 'Interviews', path: '/practice/interviews', icon: <QuestionAnswer /> },
+      { text: 'Code', path: '/practice/code', icon: <DiCode size={iconSize} /> },
+      { text: 'Interviews', path: '/practice/interviews', icon: <QuestionAnswer style={{ fontSize: iconSize }} /> },
     ],
   },
   {
     text: 'Shortcuts',
-    icon: <AddCircleOutline />,
+    icon: <GiKeyboard style={{ fontSize: iconSize }} />,
     path: '/shortcuts',
     // Add more children if needed
   },
